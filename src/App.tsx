b76905d60423826fb975951ab675ba3b03f4005a
chandancant <chandan.pandey@cantechnologies.co.in>
2022-04-04 01:07:53 +0530

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [name,SetName] = useState('')
+  const [password,SetPassword] = useState('')
+
+ const forrmValue=()=>{
+  
+ let data={
+name:name,
+password:password
+  }
+  console.log("formdata",data);
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form>
+        <label>
+          Name:
+          <input type="text" onChange={(e)=>SetName(e.target.value)} />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" onChange={(e)=>SetPassword(e.target.value)} />
+        </label>
+        <input type="button" value="Submit" onClick={()=>forrmValue()} />
+      </form>
+   <p>
+     {name}
+   </p>
     </div>
   );
 }
