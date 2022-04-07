8de01e38bee2ebfb81c754eb539ad57aa778ca57
chandancant <chandan.pandey@cantechnologies.co.in>
2022-04-07 12:31:15 +0530

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,122 @@
 import React,{useState} from 'react';
-
 import './App.css';
 
 function App() {
 
   const [name,SetName] = useState('')
+  const [mobilenumber,SetMobilenumber] = useState('')
+  const [emailid,SetEmailid] = useState('')
+  const [emaildate,SetDate] = useState('')
+  const[week,SetWeek]=useState('')
+  const[file,SetFile]=useState('')
+  const[radio,SetRadio]=useState('')
   const [password,SetPassword] = useState('')
+  const[reset,SetReset]=useState('')
+  
 
  const forrmValue=()=>{
   
  let data={
 name:name,
-password:password
+emailid:emailid,
+mobilenumber:mobilenumber,
+date:Date,
+week:week,
+file:file,
+radio:radio,
+password:password,
+reset:reset
+
   }
-  console.log("formdata",data);
-  
+ 
+  console.log("forrmdata",data);
   }
   return (
-    <div className="App">
+    <div className="">
      <form>
-        <label>
+     <h1 className='primary'> Practices of Input Field</h1>
+     
+        <label className='center' >
           Name:
-          <input type="text" onChange={(e)=>SetName(e.target.value)} />
+         <input type="text" style={{color: "red"}}  onChange={(e)=>SetName(e.target.value)} />
         </label>
+         <br></br>
 
+
+        <label>
+          MobileNomber:
+          <input type="number" onChange={(e)=>SetEmailid(e.target.value)} />
+        </label>
+        <br></br>
+
+        <label>
+          Emailid:
+          <input type="emailid" onChange={(e)=>SetEmailid(e.target.value)} />
+        </label><br></br>
+
+        <label>
+          Date:
+          <input type="date" onChange={(e)=>SetDate(e.target.value)} />
+        </label><br></br>
+       <label> 
+         week:
+          <input type ="week" onChange={(e)=>SetWeek(e.target.value)}/>
+          </label> <br></br>         
+           
+
+
+        <label>
+          <input type= "file" onChange={(e)=>SetFile(e.target.value)}/>
+        </label><br></br>
+
+       <label>
+           <input type ="radio"onChange ={(e)=>SetRadio(e.target.value)}/> 
+       </label><label>HTML</label><br></br>
+
+       
+       <label>  
+           <input type ="radio"onChange ={(e)=>SetRadio(e.target.value)}/> 
+       </label><label>CSS</label><br></br>
+ 
+       
+        
+     
         <label>
           Password:
           <input type="password" onChange={(e)=>SetPassword(e.target.value)} />
         </label>
+        <br></br>
+
+
         <input type="button" value="Submit" onClick={()=>forrmValue()} />
+        <br></br>
+
+        <label> 
+          
+          <input type="reset" onChange={(e)=>SetReset(e.target.value)}/> 
+        </label>
+        
       </form>
    <p>
      {name}
+{mobilenumber}
+
    </p>
+
+
+
+   <div className="container">
+  <div className="row">
+    <label htmlFor="">First namessss</label>
+    <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+  </div>/
+  <div className="row">
+    <label> Last name</label>
+    <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+  </div>
+</div>
+
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,9 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "material-ui"
